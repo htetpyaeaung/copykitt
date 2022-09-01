2eeb4d205b4eabe3ce6363791dd915aefb8b07d6
Htet Pyae Aung <aunhy002@mymail.unisa.edu.au>
2022-09-01 22:53:11 +0930

    Add argument to the python runtime

--- app/copykitt.py
+++ app/copykitt.py
@@ -2,15 +2,36 @@ from cmd import PROMPT
 import os
 from urllib import response
 import openai
+import argparse
 
 
-openai.api_key = os.getenv(
-    "OPENAI_API_KEY")
+def main():
+    print("Running Copy Kitt!")
 
-subject = "coffee"
-prompt = f"Generate upbeat branding snippet for {subject}"
+    parser = argparse.ArgumentParser()
+    # 1. name of variable that will get the user's argument
+    # 2. the parameter
+    # 3. value type of the argument
+    # 4. whether the argument is required or not
+    parser.add_argument("--input", "-i", type=str, required=True)
+    args = parser.parse_args()
+    user_input = args.input
 
-response = openai.Completion.create(
-    engine="text-davinci-002", prompt=prompt, max_tokens=36)
+    print(f"User Input:: {user_input}")
+    pass
 
-print(response)
+
+# # Load API Key into environment variable
+# openai.api_key = os.getenv(
+#     "OPENAI_API_KEY")
+
+# subject = "coffee"
+# prompt = f"Generate upbeat branding snippet for {subject}"
+
+# response = openai.Completion.create(
+#     engine="text-davinci-002", prompt=prompt, max_tokens=36)
+
+# print(response)
+
+if __name__ == "__main__":
+    main()
